61bea6ff874286598faf835c548363780b13cd95
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-28 18:26:55 -0500

    Incorporando clase Alumno en models.py, todos los datos del estudiante

--- mi_proyecto/gestion_Universitaria/models.py
+++ mi_proyecto/gestion_Universitaria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+  cui = models.IntegerField(validators=[MinValueValidator(10000000), MaxValueValidator(99999999)], unique=True)
+  nombre = models.CharField(max_length=100)
+  apellidos = models.CharField(max_length=100)
+  edad = models.IntegerField(max_value=100)
+  dni = models.IntegerField(validators=[MinValueValidator(10000000), MaxValueValidator(99999999)])
+  
+  def __str__(self):
+    return f"{self.nombre} {self.apellidos}"
