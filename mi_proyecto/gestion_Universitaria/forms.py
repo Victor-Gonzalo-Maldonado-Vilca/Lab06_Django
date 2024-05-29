0eb6bcd3e66c0f3c9c8a1ca5d997a03c0a687a61
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-28 19:15:20 -0500

    Agregando clases en los formularios que se encargaran de ingresar datos, todavía esta incompleto

--- mi_proyecto/gestion_Universitaria/forms.py
+++ mi_proyecto/gestion_Universitaria/forms.py
@@ -1,2 +1,21 @@
 import django import forms
 import .models import Alumno, Curso, NotasAlumnosPorCurso
+
+class AlumnoForm(forms.ModelForm):
+  
+  class Meta:
+    model = Alumno
+    fields = ['cui', 'nombre', 'apellidos', 'edad', 'dni']
+    
+class CursoForm(forms.ModelForm):
+
+  class Meta:
+    model = Curso
+    fields = ['codigo', 'nombre']
+    
+class NotasAlumnosPorCursoForm(forms.ModelForm):
+  alumno = forms.ModelChoiceField(queryset=Alumno.objects.all(), label='Alumno')
+  
+  class Meta:
+    model = NotasAlumnosPorCurso
+    fields = ['alumno', 'curso', 'notas']
