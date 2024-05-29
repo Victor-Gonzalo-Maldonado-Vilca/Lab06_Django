ea319936baf5de23efe59f8c42c76c20c650f08f
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-28 20:28:51 -0500

    Incorporando urls.py a la aplicación gestion_Universitaria

---
+++ mi_proyecto/gestion_Universitaria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('alumnos/', views.agregar_alumno, name='agregar_alumno'),
+  path('cursos/', views.agregar_curso, name='agregar_curso'),
+  path('notas/', views.agregar_nota, name='agregar_nota'),
+]
