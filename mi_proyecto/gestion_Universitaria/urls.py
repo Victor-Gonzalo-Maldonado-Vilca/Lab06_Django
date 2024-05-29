39fed51fa66284907b35d05082e044c7134c6d35
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-28 20:36:43 -0500

    Solucionando error se coloco mal la funcion de notas en urls

--- mi_proyecto/gestion_Universitaria/urls.py
+++ mi_proyecto/gestion_Universitaria/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('alumnos/', views.agregar_alumno, name='agregar_alumno'),
   path('cursos/', views.agregar_curso, name='agregar_curso'),
-  path('notas/', views.agregar_nota, name='agregar_nota'),
+  path('notas/', views.agregar_notas_alumnos_por_curso, name='agregar_nota'),
 ]
