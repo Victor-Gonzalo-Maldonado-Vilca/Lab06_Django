4215720482649381531110bba4616baa18a84049
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-28 20:53:59 -0500

    Colocando mensajes en los input number

--- mi_proyecto/gestion_Universitaria/models.py
+++ mi_proyecto/gestion_Universitaria/models.py
@@ -3,17 +3,33 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Alumno(models.Model):
-  cui = models.IntegerField(validators=[MinValueValidator(10000000), MaxValueValidator(99999999)], unique=True)
+  cui = models.IntegerField(
+    validators=[
+      MinValueValidator(10000000, message="Se requiere 8 digitos"), 
+      MaxValueValidator(99999999, message="Se requiere 8 digitos")
+    ], 
+    unique=True
+  )
   nombre = models.CharField(max_length=100)
   apellidos = models.CharField(max_length=100)
   edad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
-  dni = models.IntegerField(validators=[MinValueValidator(10000000), MaxValueValidator(99999999)])
+  dni = models.IntegerField(
+    validators=[
+      MinValueValidator(10000000, message="Se requiere 8 digitos"), 
+      MaxValueValidator(99999999, message="Se requiere 8 digitos")
+    ]
+  )
   
   def __str__(self):
     return f"{self.nombre} {self.apellidos}"
 
 class Curso(models.Model):
-  codigo = models.IntegerField(validators=[MinValueValidator(1000000), MaxValueValidator(9999999)])
+  codigo = models.IntegerField(
+    validators=[
+      MinValueValidator(1000000, message="Se requiere 7 digitos"), 
+      MaxValueValidator(9999999, message="Se requiere 7 digitos")
+    ]
+  )
   nombre = models.CharField(max_length=100)
   
   def __str__(self):
